a4e9a4686fdde7be53000637a8f8e25d2b4cbc9d
3LOK <yoava@wix.com>
2017-01-10 08:59:42 +0200

    exporting the testkit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as helpers from './helpers';
 import * as fixtures from './fixtures';
+import * as testkit from './testkit';
 
 export { default as WixRestaurantsClient } from './client/WixRestaurantsClient.js';
-export { helpers, fixtures };
+export { helpers, fixtures, testkit };
