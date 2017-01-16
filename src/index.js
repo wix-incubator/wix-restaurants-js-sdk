a6d03f6efb525c12bc9bb525b001063ac143a1e4
Jonathan Adler <jonathana@wix.com>
2017-01-16 17:03:34 +0200

    remove testkit from index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import * as helpers from './helpers';
 import * as fixtures from './fixtures';
-import * as testkit from './testkit';
 
 export { default as WixRestaurantsClient } from './client/WixRestaurantsClient.js';
-export { helpers, fixtures, testkit };
+export { helpers, fixtures };
