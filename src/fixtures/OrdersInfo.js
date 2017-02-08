3078e09339c4b0bfa75a7be4b56ecf47ec2ebbdf
3LOK <yoava@wix.com>
2017-02-08 13:18:10 -1000

    fixed OrdersInfo fixture

--- src/fixtures/OrdersInfo.js
+++ src/fixtures/OrdersInfo.js
@@ -15,7 +15,33 @@ export default function createOrdersInfo() {
             return this;
         },
         setFutureDelayMins({min, max}) {
-            fixture.futureDelayMins = {min, max};
+            fixture.future = fixture.future || {};
+            fixture.future.delayMins = {min, max};
+            return this;
+        },
+        disableFutureOrders() {
+            fixture.future = fixture.future || {};
+            fixture.future.disabled = true;
+            return this;
+        },
+        enableFutureOrders() {
+            fixture.future = fixture.future || {};
+            delete fixture.future.disabled;
+            return this;
+        },
+        disableAsapOrders() {
+            fixture.asap = fixture.asap || {};
+            fixture.asap.disabled = true;
+            return this;
+        },
+        enableAsapOrders() {
+            fixture.asap = fixture.asap || {};
+            delete fixture.asap.disabled;
+            return this;
+        },
+        setAsapPreOrderMins(mins) {
+            fixture.asap = fixture.asap || {};
+            fixture.asap.preOrderMins = mins;
             return this;
         },
         val() {
