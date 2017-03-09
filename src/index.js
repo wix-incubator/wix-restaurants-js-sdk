538f174617a329af775353c56da9a9f5093d8276
Ohad Raz <ohad.raz@gmail.com>
2017-03-09 14:21:35 +0200

    Exporting WixRestaurantsDriver as well

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import * as helpers from './helpers';
 import * as fixtures from './fixtures';
 
 export { default as WixRestaurantsClient } from './client/WixRestaurantsClient.js';
+export { default as WixRestaurantsDriver } from './testkit/WixRestaurantsDriver.js';
+        
 export { helpers, fixtures };
