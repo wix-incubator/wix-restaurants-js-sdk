207bca214f6c09f8d06e54e68a4985b8dd1e37b7
Danny Leshem <dleshem@gmail.com>
2017-03-28 11:17:53 +0300

    Removed redundant code.

--- test/testkit/CommonProtocolDriver.test.js
+++ test/testkit/CommonProtocolDriver.test.js
@@ -1,18 +1,9 @@
 import { expect } from 'chai';
-import { XMLHttpRequest } from 'xhr2';
 import {testkit} from '../../src/index';
 
 const {CommonProtocolDriver} = testkit;
 
 describe('CommonProtocolDriver', () => {
-    before(() => {
-        global.XMLHttpRequest = XMLHttpRequest;
-    });
-
-    after(() => {
-        global.XMLHttpRequest = null;
-    });
-
     it('is exported', () => {
         expect(CommonProtocolDriver).to.be.ok;
 
