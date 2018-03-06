bdac9f6aac57695522fd885690461e249a71b22d
Danny Leshem <dleshem@gmail.com>
2018-03-06 17:36:31 +0200

    Export createRestClient

--- src/clients/index.js
+++ src/clients/index.js
@@ -1,7 +1,9 @@
 import { default as WixRestaurantsClient, Endpoints } from './WixRestaurantsClient';
 import { default as WixRestaurantsAnalyticsClient, BaseUrls as AnalyticsBaseUrls, Metrics, Periods, Platforms, Statuses } from './WixRestaurantsAnalyticsClient';
+import { createRestClient } from './RestClient';
 
 export {
     WixRestaurantsClient, Endpoints,
-    WixRestaurantsAnalyticsClient, AnalyticsBaseUrls, Metrics, Periods, Platforms, Statuses
+    WixRestaurantsAnalyticsClient, AnalyticsBaseUrls, Metrics, Periods, Platforms, Statuses,
+    createRestClient
 };
