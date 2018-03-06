33b3802504867c724e76cea3ab50234aed77a1ae
Jonathan Adler <jonathana@wix.com>
2018-03-06 13:00:08 +0200

    add exported consts to index

--- src/clients/index.js
+++ src/clients/index.js
@@ -1,7 +1,7 @@
 import { default as WixRestaurantsClient, Endpoints } from './WixRestaurantsClient';
-import { default as WixRestaurantsAnalyticsClient, BaseUrls as AnalyticsBaseUrls, Metrics, Periods } from './WixRestaurantsAnalyticsClient';
+import { default as WixRestaurantsAnalyticsClient, BaseUrls as AnalyticsBaseUrls, Metrics, Periods, Platforms, Statuses } from './WixRestaurantsAnalyticsClient';
 
 export {
     WixRestaurantsClient, Endpoints,
-    WixRestaurantsAnalyticsClient, AnalyticsBaseUrls, Metrics, Periods
+    WixRestaurantsAnalyticsClient, AnalyticsBaseUrls, Metrics, Periods, Platforms, Statuses
 };
